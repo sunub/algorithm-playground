e307a4351ee9619d32f86810f983db1ffbbd47f5
sunub <bsc5672@gmail.com>
2023-06-12 01:23:54 +0900

    Add resolve dayily leetcode problems.

--- hardToUnderstand/binarySearch/1146.ts
+++ hardToUnderstand/binarySearch/1146.ts
@@ -42,3 +42,95 @@ class SnapshotArray {
         return id === -1 ? 0 : element[id][0]
     }
 }
+
+class SnapshotArray2 {
+    elements: number[][][]
+    snapId: number
+    constructor(length: number) {
+        this.elements = Array(length)
+            .fill(null)
+            .map((el) => [])
+        this.snapId = 0
+    }
+    set(index: number, val: number) {
+        this.elements[index].push([val, this.snapId])
+    }
+    snap() {
+        ++this.snapId
+        return this.snapId - 1
+    }
+    get(index: number, snap_id: number) {
+        const element = this.elements[index]
+        let left = 0,
+            right = element.length - 1,
+            mid,
+            id = -1
+
+        while (left <= right) {
+            mid = left + Math.floor((right - left) / 2)
+            if (element[mid][1] <= snap_id) {
+                id = mid
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        return id === -1 ? 0 : element[id][0]
+    }
+}
+
+/**
+ * @param {number} length
+ */
+var SnapshotArray2 = function (length) {
+    this.el = Array.from({ length: length }, (v, _) => (v = new Map()))
+    this.id = 0
+}
+
+/**
+ * @param {number} index
+ * @param {number} val
+ * @return {void}
+ */
+SnapshotArray.prototype.set = function (index, val) {
+    this.el[index].set(this.id, val)
+}
+
+/**
+ * @return {number}
+ */
+SnapshotArray.prototype.snap = function () {
+    this.id += 1
+    return this.id - 1
+}
+
+/**
+ * @param {number} index
+ * @param {number} snap_id
+ * @return {number}
+ */
+SnapshotArray.prototype.get = function (index, snap_id) {
+    const element = Array.from(this.el[index].keys())
+    let left = 0,
+        right = element.length,
+        mid = -1,
+        id = -1
+    while (left <= right) {
+        mid = left + Math.floor((right - left) / 2)
+        if (element[mid] <= snap_id) {
+            id = element[mid]
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return id === -1 ? 0 : this.el[index].get(id)
+}
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * var obj = new SnapshotArray(length)
+ * obj.set(index,val)
+ * var param_2 = obj.snap()
+ * var param_3 = obj.get(index,snap_id)
+ */
