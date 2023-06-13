e2c8e01177e5d78b254fe38fd443b5be82ac7b0a
sunub <bsc5672@gmail.com>
2023-06-13 23:34:24 +0900

    Add resolve dayily leetcode problems.

--- problems/hashtable/2352.ts
+++ problems/hashtable/2352.ts
@@ -40,3 +40,63 @@ function mySolution(grid: number[][]): number {
 
     return answer
 }
+
+function equalPairs(grid: number[][]): number {
+    const myTrie = new Trie()
+    let count = 0,
+        n = grid.length
+
+    for (let row of grid) {
+        myTrie.insert(row)
+    }
+
+    for (let c = 0; c < n; ++c) {
+        let colArray = Array.from({ length: n }, (v, _) => (v = 0))
+        for (let r = 0; r < n; ++r) {
+            colArray[r] = grid[r][c]
+        }
+        count += myTrie.search(colArray)
+    }
+
+    return count
+}
+
+class TrieNode {
+    count: number
+    children: Map<number, TrieNode>
+
+    constructor() {
+        this.count = 0
+        this.children = new Map()
+    }
+}
+
+class Trie {
+    trie: TrieNode
+    constructor() {
+        this.trie = new TrieNode()
+    }
+
+    insert(array: number[]) {
+        let myTrie = this.trie
+        for (let num of array) {
+            if (!myTrie.children.has(num)) {
+                myTrie.children.set(num, new TrieNode())
+            }
+            myTrie = myTrie.children.get(num)!
+        }
+        myTrie.count += 1
+    }
+
+    search(array: number[]) {
+        let myTrie = this.trie
+        for (let num of array) {
+            if (myTrie.children.has(num)) {
+                myTrie = myTrie.children.get(num)!
+            } else {
+                return 0
+            }
+        }
+        return myTrie.count
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,61 @@
 function equalPairs(grid: number[][]): number {
-    const n = grid.length
-    let answer = 0
-    const memo = new Map()
+    const myTrie = new Trie()
+    let count = 0,
+        n = grid.length
 
-    for (let i = 0; i < n; i++) {
-        let rowNum = grid[i].join(" ")
-        for (let j = 0; j < n; j++) {
-            let colNum = getColNum(0, j)
-            if (rowNum === colNum) {
-                answer += 1
+    for (let row of grid) {
+        myTrie.insert(row)
+    }
+
+    for (let c = 0; c < n; ++c) {
+        let colArray = Array.from({ length: n }, (v, _) => (v = 0))
+        for (let r = 0; r < n; ++r) {
+            colArray[r] = grid[r][c]
+        }
+        count += myTrie.search(colArray)
+    }
+
+    return count
+}
+
+class TrieNode {
+    count: number
+    children: Map<number, TrieNode>
+
+    constructor() {
+        this.count = 0
+        this.children = new Map()
+    }
+}
+
+class Trie {
+    trie: TrieNode
+    constructor() {
+        this.trie = new TrieNode()
+    }
+
+    insert(array: number[]) {
+        let myTrie = this.trie
+        for (let num of array) {
+            if (!myTrie.children.has(num)) {
+                myTrie.children.set(num, new TrieNode())
+            }
+            myTrie = myTrie.children.get(num)!
+        }
+        myTrie.count += 1
+    }
+
+    search(array: number[]) {
+        let myTrie = this.trie
+        for (let num of array) {
+            if (myTrie.children.has(num)) {
+                myTrie = myTrie.children.get(num)!
+            } else {
+                return 0
             }
         }
+        return myTrie.count
     }
-
-    function getColNum(col: number, row: number) {
-        if (memo.has(col)) {
-            return memo.get(col)
-        }
-
-        let colNum = []
-        while (col < n) {
-            colNum.push(grid[col][row])
-            col += 1
-        }
-        memo.set(col, colNum)
-        return colNum.join(" ")
-    }
-
-    return answer
 }
 
 console.log(
