c02d5444c43730b142ea95c0fd337a183fedb870
sunub <bsc5672@gmail.com>
2023-07-13 22:20:26 +0900

    Add resolve dayily leetcode problems

--- problems/graph/207.ts
+++ problems/graph/207.ts
@@ -1,3 +1,16 @@
+// 207. Course Schedule
+
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false.
+
+// Input: numCourses = 2, prerequisites = [[1,0]]
+// Output: true
+
+// Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+// Output: false
+
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     const map = new Map()
 
@@ -44,10 +57,3 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
         return false
     }
 }
-
-console.log(
-    canFinish(2, [
-        [1, 0],
-        [0, 1],
-    ])
-)
