95029f47a109593bc342293448cfa42b34143f17
sunub <bsc5672@gmail.com>
2023-07-15 21:00:50 +0900

    Add resolve dayily leetcode problems

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,27 @@ var longestSubsequence = function (arr: number[], difference: number) {
     return answer
 }
 console.log(longestSubsequence([1, 5, 7, 8, 5, 3, 4, 2, 1], -2))
+
+console.log(new Date("2023-06-12").getTime())
+console.log(new Date("2023-06-13").getTime())
+
+const b = [
+    {
+        date: "2023-06-12",
+    },
+    {
+        date: "2023-06-13",
+    },
+    {
+        date: "2023-06-15",
+    },
+    {
+        date: "2023-06-18",
+    },
+]
+
+console.log(
+    b.sort((a, b) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime()
+    })
+)
