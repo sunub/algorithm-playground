71f4f62879d7b844cc74e47167a1e6b0fcea02e5
sunub <bsc5672@gmail.com>
2023-08-01 23:11:37 +0900

    Add resolve daily leetcode problems

--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var minimumDeleteSum = function (s1, s2) {
-    const map = new Map();
-    backtracking(0, "");
-    function backtracking(start, key) {
-        if (map.has(key)) {
-            return map.get(key);
-        }
-        if (start >= s2.length) {
+var combine = function (n, k) {
+    function* comb(arr, index) {
+        if (arr.length === k) {
+            yield arr;
             return;
         }
-        for (let i = start; i < s2.length; i++) {
-            key += s2[i];
-            backtracking(i + 1, key);
-            key = key.length > 1 ? key.slice(0, key.length - 1) : key[0];
+        for (let i = index; i < n + 1; i++) {
+            yield* comb([...arr, i], i + 1);
         }
+        return;
     }
+    return [...comb([], 1)];
 };
-function mapping(map, s) {
-    return map.has(s) ? map.set(s, map.get(s) + 1) : map.set(s, 1);
-}
-console.log(minimumDeleteSum("delete", "leet"));
-const a = "asd";
-console.log(a.slice(0, a.length - 1));
+console.log(combine(4, 2));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,2 @@
-// var minimumDeleteSum = function(s1: string, s2: string) {
-//     const memo = new Map();
-
-//     function search(start: number, key: string) {
-//         if(memo.has(start)) {
-//             return memo.get(start)
-//         }
-
-//         for(let i = start; i < s2.length; i++) {
-//             key += s2[i];
-//             memo.set(key, deleteCompareString(key))
-//             search(i + 1, key);
-//             key = key.substring(0, key.length - 1)
-//         }
-//     }
-
-//     function deleteCompareString(target: string) {
-
-//     }
-// };
-// function mapping(map: Map<string, number>, s: string) {
-//     return map.has(s) ? map.set(s, map.get(s)! + 1) : map.set(s, 1)
-// }
-// console.log(minimumDeleteSum("delete", "leet"))
-
-const a = "e";
-console.log(a.substring(0, a.length - 1))
+var minimumDeleteSum = function (s1, s2) {}
+console.log(minimumDeleteSum("delete", "leet"))
