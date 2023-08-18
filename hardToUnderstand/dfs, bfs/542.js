c28aa7a494468016f2e07bac9758f730ba460433
sunub <bsc5672@gmail.com>
2023-08-18 22:50:54 +0900

    feat: Add resolve daily leetcode problem

--- hardToUnderstand/dfs, bfs/542.js
+++ hardToUnderstand/dfs, bfs/542.js
@@ -48,6 +48,7 @@ var updateMatrix = function (mat) {
 
             if (valid(nextRow, nextCol) && !visit[nextRow][nextCol]) {
                 visit[nextRow][nextCol] = true
+                // 내가 풀었던 문제에서 steps의 값이 각각의 값으로 서로 참조하지 않고 변수가 저장되지 않았던 문제를 해결할 수 있는 방법으로 사용되었다. 변수의 값들은 서로 참조가 되므로 이런 식으로 queue를 이용하여 돌아갈 수 있게끔 저장하는 방법을 사용해야 한다는 것을 기억하자.
                 queue.push([nextRow, nextCol, steps + 1])
                 mat[nextRow][nextCol] = steps + 1
             }
@@ -58,5 +59,41 @@ var updateMatrix = function (mat) {
 
     function valid(row, col) {
         return 0 <= row && row < m && 0 <= col && col < n
+    }
+}
+
+function myAnswer(mat) {
+    const m = mat.length
+    const n = mat[0].length
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 1) {
+                const visit = Array.from({ length: m }, () =>
+                    Array.from({ length: n }, () => false)
+                )
+                mat[i][j] = bfs(i, j, visit)
+            }
+        }
+    }
+
+    return mat
+
+    function bfs(i, j, visit) {
+        if (i < 0 || i + 1 > m || j < 0 || j + 1 > n || visit[i][j]) {
+            return Number.MAX_SAFE_INTEGER
+        }
+
+        if (mat[i][j] === 0) {
+            return 0
+        }
+
+        visit[i][j] = true
+        const a = bfs(i - 1, j, visit)
+        const b = bfs(i + 1, j, visit)
+        const c = bfs(i, j - 1, visit)
+        const d = bfs(i, j + 1, visit)
+        // 카운트 값이 저장이 되지 않고 돌아가지 않는다. 내가 원하는 것은 한번 돌때마다 이전의 값으로 돌아가는 것이지만 값들이 이전으로 돌릴때 서로 참조가 되어서 그런지 돌아가지 않고 저장된 값에서 계속해서 되니 내가 원하는 값이 되지 않는다.
+        return 1 + Math.min(a, b, c, d)
     }
 }
--- problems/graph/1615.js
+++ problems/graph/1615.js
@@ -1,3 +1,17 @@
+// 1615. Maximal Network Rank
+
+// There is an infrastructure of n cities with some number of roads connecting these cities. Each roads[i] = [ai, bi] indicates that there is a bidirectional road between cities ai and bi.
+
+// The network rank of two different cities is defined as the total number of directly connected roads to either city. If a road is directly connected to both cities, it is only counted once.
+
+// The maximal network rank of the infrastructure is the maximum network rank of all pairs of different cities.
+
+// Given the integer n and the array roads, return the maximal network rank of the entire infrastructure.
+
+// Input: n = 4, roads = [[0,1],[0,3],[1,2],[1,3]]
+// Output: 4
+// Explanation: The network rank of cities 0 and 1 is 4 as there are 4 roads that are connected to either 0 or 1. The road between 0 and 1 is only counted once.
+
 var maximalNetworkRank = function (n, roads) {
     const adj = new Map()
     const networkRank = new Map()
@@ -39,14 +53,3 @@ var maximalNetworkRank = function (n, roads) {
             : networkRank.set(key, 1)
     }
 }
-
-console.log(
-    maximalNetworkRank(5, [
-        [0, 1],
-        [0, 3],
-        [1, 2],
-        [1, 3],
-        [2, 3],
-        [2, 4],
-    ])
-)
