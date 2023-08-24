d0d91093a7f6a5dda3843dff30e08e67f235e8b1
sunub <bsc5672@gmail.com>
2023-08-24 22:08:21 +0900

    feat: Add resolve daily leetcode problem

--- dist/app.js
+++ dist/app.js
@@ -1,42 +1,56 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var reorganizeString = function (s) {
-    if (s.length <= 1)
-        return s;
-    let sArray = s.split("");
-    const n = s.length;
-    const queue = [];
-    let i = 1;
-    while (i < sArray.length) {
-        if (sArray[i] === sArray[i - 1]) {
-            queue.push(s[i]);
-            sArray = [...sArray.slice(0, i), ...sArray.slice(i + 1, n)];
-            continue;
+var fullJustify = function (words, maxWidth) {
+    const n = words.length;
+    const fullJustified = [];
+    const answer = [];
+    let lineJustify = [];
+    let lineWidth = 0;
+    for (let i = 0; i < n; i++) {
+        lineJustify.push(words[i]);
+        lineWidth += words[i].length;
+        if (lineWidth === maxWidth) {
+            fullJustified.push([...lineJustify, `${lineWidth}`]);
+            [lineJustify, lineWidth] = [[], 0];
         }
-        i += 1;
-    }
-    while (queue.length) {
-        let curr = queue.pop();
-        for (let i = 0; i <= sArray.length; i++) {
-            if (i === 0 && sArray[i] !== curr) {
-                sArray.unshift(curr);
-                break;
-            }
-            if (i === sArray.length) {
-                curr !== sArray[i - 1] ? sArray.push(curr) : null;
-                break;
-            }
-            if (sArray[i - 1] !== curr && sArray[i] !== curr) {
-                sArray = [
-                    ...sArray.slice(0, i),
-                    curr,
-                    ...sArray.slice(i, sArray.length),
-                ];
-                break;
-            }
+        else if (lineWidth > maxWidth) {
+            const exceed = lineJustify.pop();
+            fullJustified.push([...lineJustify, `${lineWidth - exceed.length}`]);
+            [lineJustify, lineWidth] = [[exceed], exceed.length];
+        }
+        if (i === n - 1) {
+            fullJustified.push([...lineJustify, `${lineWidth}`]);
         }
     }
-    return sArray.length === s.length ? sArray.join("") : "";
+    for (let i = 1; i < fullJustified.length; i++) {
+        let longestLength = +fullJustified[i - 1].pop();
+        const currLength = +fullJustified[i].pop();
+        const word = fullJustified[i - 1].pop();
+        if (currLength + word.length <= maxWidth) {
+            fullJustified[i].unshift(word);
+            longestLength -= word.length;
+            fullJustified[i].push(`${currLength + word.length}`);
+        }
+        else {
+            fullJustified[i - 1].unshift(word);
+            fullJustified[i].push(`${currLength}`);
+        }
+    }
+    console.log(answer);
 };
-console.log(reorganizeString("baabb"));
+function fillJustifyWords(words, remainSpace, maxWidth) {
+    let line = "";
+    for (let i = 0; i < words.length; i++) {
+        line += words[i];
+        if (i !== words.length - 1) {
+            let j = 0;
+            while (j < remainSpace) {
+                line += " ";
+                j += 1;
+            }
+        }
+    }
+    return line.length < maxWidth ? (line += " ") : line;
+}
+console.log(fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,77 @@
-var reorganizeString = function (s: string) {
-    if (s.length <= 1) return s
+import lodash from "lodash"
 
-    let sArray: string[] = s.split("")
-    const n = s.length
-    const queue: string[] = []
+var fullJustify = function (words: string[], maxWidth: number) {
+    const n = words.length
+    const fullJustified: string[][] = []
+    const answer: string[] = []
 
-    let i = 1
-    while (i < sArray.length) {
-        if (sArray[i] === sArray[i - 1]) {
-            queue.push(s[i])
-            sArray = [...sArray.slice(0, i), ...sArray.slice(i + 1, n)]
-            continue
+    let lineJustify: string[] = []
+    let lineWidth = 0
+
+    for (let i = 0; i < n; i++) {
+        lineJustify.push(words[i])
+        lineWidth += words[i].length
+
+        if (lineWidth === maxWidth) {
+            fullJustified.push([...lineJustify, `${lineWidth}`])
+            ;[lineJustify, lineWidth] = [[], 0]
+        } else if (lineWidth > maxWidth) {
+            const exceed = lineJustify.pop()!
+            fullJustified.push([...lineJustify, `${lineWidth - exceed.length}`])
+            ;[lineJustify, lineWidth] = [[exceed], exceed.length]
         }
-        i += 1
-    }
 
-    while (queue.length) {
-        let curr: string = queue.pop()!
-
-        for (let i = 0; i <= sArray.length; i++) {
-            if (i === 0 && sArray[i] !== curr) {
-                sArray.unshift(curr)
-                break
-            }
-
-            if (i === sArray.length) {
-                curr !== sArray[i - 1] ? sArray.push(curr) : null
-                break
-            }
-
-            if (sArray[i - 1] !== curr && sArray[i] !== curr) {
-                sArray = [
-                    ...sArray.slice(0, i),
-                    curr,
-                    ...sArray.slice(i, sArray.length),
-                ]
-                break
-            }
+        if (i === n - 1) {
+            fullJustified.push([...lineJustify, `${lineWidth}`])
         }
     }
 
-    return sArray.length === s.length ? sArray.join("") : ""
+    for (let i = 1; i < fullJustified.length; i++) {
+        let longestLength = +fullJustified[i - 1].pop()!
+        const currLength = +fullJustified[i].pop()!
+
+        const word = fullJustified[i - 1].pop()!
+        if (currLength + word.length <= maxWidth) {
+            fullJustified[i].unshift(word)
+            longestLength -= word.length
+            fullJustified[i].push(`${currLength + word.length}`)
+        } else {
+            fullJustified[i - 1].unshift(word)
+            fullJustified[i].push(`${currLength}`)
+        }
+    }
+
+    for(let i = 0; i < fullJustified.length; i++) {
+        let remainLength = maxWidth;
+
+        for(let j = 0; j < fullJustified[i].length; j++) {}
+    }
 }
 
-console.log(reorganizeString("baabb"))
+function fillJustifyWords(
+    words: string[],
+    remainSpace: number,
+    maxWidth: number
+) {
+    let line = ""
+
+    for (let i = 0; i < words.length; i++) {
+        line += words[i]
+
+        if (i !== words.length - 1) {
+            let j = 0
+            while (j < remainSpace) {
+                line += " "
+                j += 1
+            }
+        }
+    }
+
+    return line.length < maxWidth ? (line += " ") : line
+}
+console.log(
+    fullJustify(
+        ["This", "is", "an", "example", "of", "text", "justification."],
+        16
+    )
+)
