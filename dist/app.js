6c8f9cb3dc19a45b0d1b6573541ee77111e1ab48
sunub <bsc5672@gmail.com>
2023-08-25 23:08:44 +0900

    feat: Add resolve daily leetcode problem

--- dist/app.js
+++ dist/app.js
@@ -1,56 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var fullJustify = function (words, maxWidth) {
-    const n = words.length;
-    const fullJustified = [];
-    const answer = [];
-    let lineJustify = [];
-    let lineWidth = 0;
-    for (let i = 0; i < n; i++) {
-        lineJustify.push(words[i]);
-        lineWidth += words[i].length;
-        if (lineWidth === maxWidth) {
-            fullJustified.push([...lineJustify, `${lineWidth}`]);
-            [lineJustify, lineWidth] = [[], 0];
-        }
-        else if (lineWidth > maxWidth) {
-            const exceed = lineJustify.pop();
-            fullJustified.push([...lineJustify, `${lineWidth - exceed.length}`]);
-            [lineJustify, lineWidth] = [[exceed], exceed.length];
-        }
-        if (i === n - 1) {
-            fullJustified.push([...lineJustify, `${lineWidth}`]);
-        }
-    }
-    for (let i = 1; i < fullJustified.length; i++) {
-        let longestLength = +fullJustified[i - 1].pop();
-        const currLength = +fullJustified[i].pop();
-        const word = fullJustified[i - 1].pop();
-        if (currLength + word.length <= maxWidth) {
-            fullJustified[i].unshift(word);
-            longestLength -= word.length;
-            fullJustified[i].push(`${currLength + word.length}`);
-        }
-        else {
-            fullJustified[i - 1].unshift(word);
-            fullJustified[i].push(`${currLength}`);
-        }
-    }
-    console.log(answer);
-};
-function fillJustifyWords(words, remainSpace, maxWidth) {
-    let line = "";
-    for (let i = 0; i < words.length; i++) {
-        line += words[i];
-        if (i !== words.length - 1) {
-            let j = 0;
-            while (j < remainSpace) {
-                line += " ";
-                j += 1;
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function (s1, s2, s3) {
+    let m = s1.length, n = s2.length, l = s3.length;
+    if (m + n !== l)
+        return false;
+    const table = Array.from({ length: m + 1 }, () => Array.from({ length: n + 1 }, () => false));
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i === 0 && j === 0) {
+                table[i][j] = true;
+            }
+            else if (i === 0) {
+                table[i][j] = table[i][j - 1] && s2[j - 1] === s3[i + j - 1];
+            }
+            else if (j === 0) {
+                table[i][j] = table[i - 1][j] && s1[i - 1] === s3[i + j - 1];
+            }
+            else {
+                table[i][j] =
+                    (table[i][j - 1] && s2[j - 1] === s3[i + j - 1]) ||
+                        (table[i - 1][j] && s1[i - 1] === s3[i + j - 1]);
             }
         }
     }
-    return line.length < maxWidth ? (line += " ") : line;
-}
-console.log(fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16));
+};
+console.log(isInterleave("aa", "ab", "abaa"));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,77 +1,35 @@
-import lodash from "lodash"
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function (s1, s2, s3) {
+    let m = s1.length,
+        n = s2.length,
+        l = s3.length
+    if (m + n !== l) return false
 
-var fullJustify = function (words: string[], maxWidth: number) {
-    const n = words.length
-    const fullJustified: string[][] = []
-    const answer: string[] = []
+    const table = Array.from({ length: m + 1 }, () =>
+        Array.from({ length: n + 1 }, () => false)
+    )
 
-    let lineJustify: string[] = []
-    let lineWidth = 0
-
-    for (let i = 0; i < n; i++) {
-        lineJustify.push(words[i])
-        lineWidth += words[i].length
-
-        if (lineWidth === maxWidth) {
-            fullJustified.push([...lineJustify, `${lineWidth}`])
-            ;[lineJustify, lineWidth] = [[], 0]
-        } else if (lineWidth > maxWidth) {
-            const exceed = lineJustify.pop()!
-            fullJustified.push([...lineJustify, `${lineWidth - exceed.length}`])
-            ;[lineJustify, lineWidth] = [[exceed], exceed.length]
-        }
-
-        if (i === n - 1) {
-            fullJustified.push([...lineJustify, `${lineWidth}`])
-        }
-    }
-
-    for (let i = 1; i < fullJustified.length; i++) {
-        let longestLength = +fullJustified[i - 1].pop()!
-        const currLength = +fullJustified[i].pop()!
-
-        const word = fullJustified[i - 1].pop()!
-        if (currLength + word.length <= maxWidth) {
-            fullJustified[i].unshift(word)
-            longestLength -= word.length
-            fullJustified[i].push(`${currLength + word.length}`)
-        } else {
-            fullJustified[i - 1].unshift(word)
-            fullJustified[i].push(`${currLength}`)
-        }
-    }
-
-    for(let i = 0; i < fullJustified.length; i++) {
-        let remainLength = maxWidth;
-
-        for(let j = 0; j < fullJustified[i].length; j++) {}
-    }
-}
-
-function fillJustifyWords(
-    words: string[],
-    remainSpace: number,
-    maxWidth: number
-) {
-    let line = ""
-
-    for (let i = 0; i < words.length; i++) {
-        line += words[i]
-
-        if (i !== words.length - 1) {
-            let j = 0
-            while (j < remainSpace) {
-                line += " "
-                j += 1
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i === 0 && j === 0) {
+                table[i][j] = true
+            } else if (i === 0) {
+                table[i][j] = table[i][j - 1] && s2[j - 1] === s3[i + j - 1]
+            } else if (j === 0) {
+                table[i][j] = table[i - 1][j] && s1[i - 1] === s3[i + j - 1]
+            } else {
+                table[i][j] =
+                    (table[i][j - 1] && s2[j - 1] === s3[i + j - 1]) ||
+                    (table[i - 1][j] && s1[i - 1] === s3[i + j - 1])
             }
         }
     }
 
-    return line.length < maxWidth ? (line += " ") : line
+    return table[m][n]
 }
-console.log(
-    fullJustify(
-        ["This", "is", "an", "example", "of", "text", "justification."],
-        16
-    )
-)
+console.log(isInterleave("aa", "ab", "abaa"))
