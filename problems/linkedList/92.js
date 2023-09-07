feb0bd88e830548f38ecf4ce43f5ef007231066b
sunub <bsc5672@gmail.com>
2023-09-07 22:49:32 +0900

    feat: Add how i resolve the leetcode problem

---
+++ problems/linkedList/92.js
@@ -0,0 +1,58 @@
+// 92. Reverse Linked List II
+
+// Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+// Example 1:
+// Input: head = [1,2,3,4,5], left = 2, right = 4
+// Output: [1,4,3,2,5]
+// Example 2:
+
+// Input: head = [5], left = 1, right = 1
+// Output: [5]
+
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function (head, left, right) {
+    if (left >= right) return head
+
+    const needToChangeNode = []
+
+    let n = head,
+        isLeft = false,
+        i = 1
+    while (n) {
+        if (!isLeft && i === left) {
+            isLeft = true
+            needToChangeNode.push(n.val)
+        } else if (isLeft) {
+            needToChangeNode.push(n.val)
+            if (i === right) {
+                break
+            }
+        }
+        n = n.next
+        i += 1
+    }
+
+    ;(isLeft = false), (i = 1)
+    for (let node = head; node !== null; node = node.next) {
+        if (i === left) {
+            isLeft = true
+            node.val = needToChangeNode.pop()
+        } else if (isLeft && needToChangeNode.length) {
+            node.val = needToChangeNode.pop()
+        }
+        i += 1
+    }
+
+    return head
+}
