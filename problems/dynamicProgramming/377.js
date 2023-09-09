4cf740f8574009af796cb7b6b8078563e36f3cbb
sunub <bsc5672@gmail.com>
2023-09-09 23:45:35 +0900

    feat: Add resolve daily leetcode problem

---
+++ problems/dynamicProgramming/377.js
@@ -0,0 +1,1 @@
+var combinationSum4 = function (nums, target) {}
--- src/app.js
+++ src/app.js
@@ -1,20 +1,2 @@
-/**
- * @param {any} o1
- * @param {any} o2
- * @return {boolean}
- */
-function helper(key, value) {
-    if (value && typeof value === "object" && !Array.isArray(value))
-        return Object.fromEntries(Object.entries(value).sort())
-    else return value
-}
-
-var areDeeplyEqual = function (o1, o2) {
-    const stringifiedO1 = JSON.stringify(o1, helper)
-    const stringifiedO2 = JSON.stringify(o2, helper)
-
-    return stringifiedO1 === stringifiedO2
-}
-console.log(
-    areDeeplyEqual({ x: null, L: [1, 2, 3] }, { x: null, L: ["1", "2", "3"] })
-)
+const b = "aa"
+console.log(b.slice(0, b.length - 1))
