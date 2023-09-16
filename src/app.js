fa6e28c31cda0d816402999c12e01626346f5706
sunub <bsc5672@gmail.com>
2023-09-16 23:01:33 +0900

    feat: Add todo job for dijkstar's Graph algorithm

--- src/app.js
+++ src/app.js
@@ -1,23 +1,47 @@
-class UnionFind {
-    constructor(size) {
-        this.group = Array.from({ length: size }, (_, i) => i);
-    }
-
-    find(node) {
-        if (this.group[node] !== node) this.find(this.group[node]);
-        return this.group[node];
-    }
-
-    union(node1, node2) {
-        const rootX = this.find(node1);
-        const rootY = this.find(node2);
-
-        if (rootX === rootY) return false;
-    }
-}
-
 /**
- * @param {number[][]} points
+ * @param {number[][]} heights
  * @return {number}
  */
-var minCostConnectPoints = function (points) {};
+var minimumEffortPath = function (heights) {
+    const m = heights.length;
+    const n = heights[0].length;
+    const dp = Array.from({ length: m }, () =>
+        Array.from({ length: n }, () => 0)
+    );
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            let curr = heights[i][j];
+
+            if (i === 0 && j === 0) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]);
+            } else if (i + 1 < m && j + 1 < n) {
+                dp[i][j] = Math.min(
+                    Math.abs(curr - heights[i + 1][j]),
+                    Math.abs(curr - heights[i][j + 1])
+                );
+            } else if (i + 1 < m) {
+                dp[i][j] = Math.max(
+                    Math.abs(curr - heights[i + 1][j]),
+                    dp[i + 1][j]
+                );
+            } else if (j + 1 < n) {
+                dp[i][j] = Math.max(
+                    Math.abs(curr - heights[i][j + 1]),
+                    dp[i][j + 1]
+                );
+            }
+        }
+    }
+    console.log(dp);
+
+    return dp[0][0];
+};
+
+console.log(
+    minimumEffortPath([
+        [1, 2, 2],
+        [3, 8, 2],
+        [5, 3, 5],
+    ])
+);
