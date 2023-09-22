5734c26547d894cef798059cb620f655dc1eb02e
sunub <bsc5672@gmail.com>
2023-09-22 22:54:53 +0900

    feat : Add resolved daily leetcode problem

---
+++ problems/sorting/392.js
@@ -0,0 +1,37 @@
+// 392. Is Subsequence
+
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+    s = s.split("");
+    t = t.split("");
+
+    let isSub = Array.from({ length: s.length }, () => false);
+    for (let i = 0; i < s.length; i++) {
+        while (t.length) {
+            if (s[i] === t[0]) {
+                isSub[i] = true;
+                t.shift();
+                break;
+            }
+            t.shift();
+        }
+    }
+    return !isSub.includes(false);
+};
