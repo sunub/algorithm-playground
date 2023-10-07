c1a18a32980af079535cfb360861a971f2e94f4c
sunub <bsc5672@gmail.com>
2023-10-07 23:43:33 +0900

    feat : (Failed to solved) Problems with using prefix sum and dynamic programming

--- src/app.js
+++ src/app.js
@@ -1,43 +1,17 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var majorityElement = function (nums) {
-    let count1 = 0;
-    let count2 = 0;
+var numOfArrays = function (n, m, k) {};
 
-    let candiate1 = null;
-    let candiate2 = null;
+function givenAlgorithm(arr) {
+    let [maxValue, maxIndex, searchCost] = [-1, -1, 0];
+    const n = arr.length;
 
-    for (const num of nums) {
-        if (candiate1 != null && candiate1 === num) {
-            count1 += 1;
-        } else if (candiate2 != null && candiate2 === num) {
-            count2 += 1;
-        } else if (count1 === 0) {
-            candiate1 = num;
-            count1 += 1;
-        } else if (count2 === 0) {
-            candiate2 = num;
-            count2 += 1;
-        } else {
-            count1 -= 1;
-            count2 -= 1;
+    for (let i = 0; i < n; i++) {
+        if (maxValue < arr[i]) {
+            maxValue = arr[i];
+            maxIndex = i;
+            searchCost = searchCost + 1;
         }
     }
 
-    const result = [];
-
-    [count1, count2] = [0, 0];
-    for (const num of nums) {
-        if (candiate1 != null && num === candiate1) count1 += 1;
-        if (candiate2 != null && num === candiate2) count2 += 1;
-    }
-
-    const n = nums.length;
-    if (count1 > Math.floor(n / 3)) result.push(candiate1);
-    if (count2 > Math.floor(n / 3)) result.push(candiate2);
-
-    return result;
-};
-console.log(majorityElement([3, 2, 3, 1, 1, 2, 2]));
+    return maxIndex;
+}
+console.log(numOfArrays(2, 3, 1));
