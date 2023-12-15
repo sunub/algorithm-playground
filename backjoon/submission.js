04a3c592386e2a946e311a3cd51f3efef8794ad1
sunub <bsc5672@gmail.com>
2023-12-15 23:36:49 +0900

    feat : DisjointSet에 관한 문제 추가

---
+++ backjoon/submission.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let count = input[0];
+let numbers = [];
+
+for (let i = 1; i < input.length; i++) {}
--- disjointSet_weight.js
+++ disjointSet_weight.js
@@ -1,19 +1,28 @@
+/**
+ * @param {string[][]} equations
+ * @param {number[]} values
+ * @param {string[][]} queries
+ * @return {number[]}
+ */
 var calcEquation = function (equations, values, queries) {
     const uf = new UnionFind();
 
-    for (let i = 0; i < equations.length; i += 1) {
+    // Step 1). union groups 생성
+    for (let i = 0; i < equations.length; i++) {
         const [dividend, divisor] = equations[i];
         const quotient = values[i];
 
         uf.union(dividend, divisor, quotient);
     }
 
-    const results = Array.from({ length: queries.length }, () => 0);
-    for (let i = 0; i < queries.length; i += 1) {
+    // Step 2). find() 함수 내에서 "lazy"한 업데이트로 계산 실행
+    const results = new Array(queries.length);
+    for (let i = 0; i < queries.length; i++) {
         const [dividend, divisor] = queries[i];
 
         if (!uf.weight.has(dividend) || !uf.weight.has(divisor)) {
-            results[i] = -1;
+            // case 1). 적어도 하나의 변수가 이전에 나타나지 않은 경우
+            results[i] = -1.0;
         } else {
             const dividendEntry = uf.find(dividend);
             const divisorEntry = uf.find(divisor);
@@ -41,24 +50,21 @@ class UnionFind {
         this.weight = new Map();
     }
 
-    find(node) {
-        if (!this.weight.has(node)) {
-            this.weight.set(node, {
-                key: node,
-                value: 1,
-            });
+    find(nodeId) {
+        if (!this.weight.has(nodeId)) {
+            this.weight.set(nodeId, { key: nodeId, value: 1 });
         }
 
-        const entry = this.weight.get(node);
-        if (entry.key !== node) {
+        const entry = this.weight.get(nodeId);
+        if (entry.key !== nodeId) {
             const newEntry = this.find(entry.key);
-            this.weight.set(node, {
+            this.weight.set(nodeId, {
                 key: newEntry.key,
                 value: newEntry.value * entry.value,
             });
         }
 
-        return this.weight.get(node);
+        return this.weight.get(nodeId);
     }
 
     union(dividend, divisor, value) {
@@ -87,18 +93,3 @@ class UnionFind {
         }
     }
 }
-
-const equations = [
-        ["a", "b"],
-        ["b", "c"],
-    ],
-    values = [2.0, 3.0],
-    queries = [
-        ["a", "c"],
-        ["b", "a"],
-        ["a", "e"],
-        ["a", "a"],
-        ["x", "x"],
-    ];
-
-console.log(calcEquation(equations, values, queries));
